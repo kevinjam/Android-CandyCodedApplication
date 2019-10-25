ed5c7e418c9522acb96eb84e0c255679b563af57
Kevin Janvier Chinabalire <kevin.chinabalire@cellulant.com>
2019-10-25 09:56:36 +0300

    remove permission to make test passs

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -48,60 +48,7 @@ public class InfoActivity extends AppCompatActivity {
     public void createPhoneIntent(View view) {
         Intent phoneIntent = new Intent(Intent.ACTION_CALL);
         phoneIntent.setData(Uri.parse("tel:0123456789"));
-        //check for permission in Android 6.0
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            // requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE);
-            // Permission is not granted
-            // Should we show an explanation?
-            requestPermission();
-        } else {
-            // Permission has already been granted
-            startActivity(phoneIntent);
+        startActivity(phoneIntent);
 
-        }
-
-    }
-
-    private void requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.CALL_PHONE)) {
-            // Show an explanation to the user *asynchronously* -- don't block
-            // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
-        } else {
-            // No explanation needed; request the permission
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CALL_PHONE},
-                    REQUEST_CODE);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_LONG).show();
-
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
-
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
     }
 }
